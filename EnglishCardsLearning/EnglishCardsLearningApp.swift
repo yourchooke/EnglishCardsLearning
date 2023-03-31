ab404853274225c586065bc4e6f747e0d0740379
Olga <olga.yourchooke@gmail.com>
2023-03-31 21:31:16 +0700

    Initial Commit

---
+++ EnglishCardsLearning/EnglishCardsLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnglishCardsLearningApp.swift
+//  EnglishCardsLearning
+//
+//  Created by Olga Yurchuk on 31.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct EnglishCardsLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
