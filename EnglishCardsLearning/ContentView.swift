383772d078197859694b92ec7c30b8ad7133218e
Olga <olga.yourchooke@gmail.com>
2023-04-14 20:26:11 +0700

    first view

--- EnglishCardsLearning/ContentView.swift
+++ EnglishCardsLearning/ContentView.swift
@@ -8,14 +8,65 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var textToTranslate = ""
+    @State private var translation = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView{
+            VStack {
+                Spacer()
+                Text("The simpliest way to learn English words")
+                    .font(.system(.largeTitle, design: .monospaced))
+                    .foregroundColor(.gray)
+                Spacer()
+                HStack{
+                    VStack{
+                        TextField("Введите слово", text: $textToTranslate)
+                            .textFieldStyle(.roundedBorder)
+                            .multilineTextAlignment(.leading)
+                        TextField("Перевод", text: $translation)
+                            .textFieldStyle(.roundedBorder)
+                            .multilineTextAlignment(.leading)
+                    }
+                    Spacer()
+                    Button("Добавить к изучению", action: addToLearnAction)
+                        .buttonStyle(.bordered)
+                } .padding()
+                Spacer(minLength: 20)
+                Text("Статистика")
+                    .font(.system(.title2))
+                Spacer()
+                HStack {
+                    Spacer()
+                    VStack {
+                        Text("Ещё учу")
+                            .padding()
+                        Text("5")
+                            .foregroundColor(.blue)
+                    }
+                    Spacer()
+                    VStack {
+                        Text("Изучено")
+                            .padding()
+                        Text("500")
+                            .foregroundColor(.blue)
+                    }
+                    Spacer()
+                }
+                Spacer()
+                Button("Начать изучение", action: startLearningAction)
+                    .buttonStyle(.borderedProminent)
+                    .frame(minWidth: 500, minHeight: 200)
+            }
+            .padding()
         }
-        .padding()
+    }
+    private func addToLearnAction(){
+        print("слово \(textToTranslate) добавлено к изучению")
+    }
+    
+    private func startLearningAction() {
+        
     }
 }
 
---
+++ EnglishCardsLearning/Services/NetworkManager.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkManager.swift
+//  EnglishCardsLearning
+//
+//  Created by Olga Yurchuk on 02.04.2023.
+//
+
+import Foundation
+
+class NetworkManager {
+    
+
+    
+}
