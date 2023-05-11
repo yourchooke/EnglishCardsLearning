6507be881c8da122426624dd3d4ddcaec07ce83c
Olga <olga.yourchooke@gmail.com>
2023-05-11 21:33:29 +0700

    adding view issue

--- EnglishCardsLearning/AddingCardView.swift
+++ EnglishCardsLearning/AddingCardView.swift
@@ -11,17 +11,25 @@ struct AddingCardView: View {
     var willLearnCount: String
     
     var body: some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 24)
-                .foregroundColor(Color(uiColor: UIColor(red: 1, green: 0.848, blue: 0.762, alpha: 1)))
-            VStack {
-                HStack {
-                    Text("Буду учить")
-                    Text(willLearnCount)
-                        .foregroundColor(Color(uiColor: UIColor(red: 0.821, green: 0.367, blue: 0.116, alpha: 1)))
-                    Spacer()
+        NavigationStack {
+            ZStack {
+                RoundedRectangle(cornerRadius: 24)
+                    .foregroundColor(Color(uiColor: UIColor(red: 1, green: 0.848, blue: 0.762, alpha: 1)))
+                VStack {
+                    HStack {
+                        Text("Буду учить")
+                        Text(willLearnCount)
+                            .foregroundColor(Color(uiColor: UIColor(red: 0.821, green: 0.367, blue: 0.116, alpha: 1)))
+                        Spacer()
+                        NavigationLink {
+                        } label: {
+                            Text("Посмотреть")
+                                .foregroundColor(Color(uiColor:  UIColor(red: 0.2, green: 0.2, blue: 0.2, alpha: 1)))
+                                .underline(true, color: Color( UIColor(red: 0.2, green: 0.2, blue: 0.2, alpha: 1)))
+                        }
+                    }
+                    .padding()
                 }
-                .padding()
             }
         }
     }
